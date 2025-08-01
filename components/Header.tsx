32b9ba2adfec05470e8ddbdec8605e055227e47a
Davi P. Oliveira <dpo.dev11@gmail.com>
2025-08-01 19:59:33 -0300

    added statscard and tripcard

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,8 +12,18 @@ const Header = ({ title, description }: Props) => {
   return (
     <header className="header">
        <article>
-          <h1 className={cn("text-dark-100", location.pathname === '/' ? 'text-2xl md:text-4xl font-bold' : 'text-xl md:text-2xl font-semibold' )}>{title}</h1>
-          <p className={cn("text-gray-100 font-normal", location.pathname === '/' ? 'text-base md:text-lg' : 'text-sm md:text-lg' )}>{description}</p>
+          <h1 className={cn("text-dark-100", location.pathname === '/' ? 
+                                        'text-2xl md:text-4xl font-bold' : 
+                                        'text-xl md:text-2xl font-semibold' )}
+                                        >
+                                          {title}
+                                        </h1>
+          <p className={cn("text-gray-100 font-normal", location.pathname === '/' ? 
+                                        'text-base md:text-lg' : 
+                                        'text-sm md:text-lg' )}
+                                       >
+                                        {description}
+                                      </p>
        </article>
     </header>
   )
